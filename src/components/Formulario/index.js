153fae3ca31188c131a0be300272057c39ee72fc
leobuck <leobuck12@gmail.com>
2024-01-28 19:13:47 -0300

    Lista dos times

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,7 +1,18 @@
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = () => {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "DevOps",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
+
   return (
     <section className="formulario">
       <form>
@@ -9,6 +20,7 @@ const Formulario = () => {
         <CampoTexto label="Nome" placeholder="Digite o seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={times} />
       </form>
     </section>
   );
