8799c805dce21cc004ec2b8a1bd086f47143dc52
leobuck <leobuck12@gmail.com>
2024-12-01 12:32:22 -0300

    Card de colaborador

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,17 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img src="https://github.com/leobuck.png" alt="Leonardo" />
+      </div>
+      <div className="rodape">
+        <h4>Leonardo</h4>
+        <h5>Desenvolvedor</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
