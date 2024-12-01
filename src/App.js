b8c411edac9db4afba098b533b098d9dd2d88d5f
leobuck <leobuck12@gmail.com>
2024-12-01 13:45:31 -0300

    Desafio: componente do rodapé

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Banner from "./components/Banner";
 import Formulario from "./components/Formulario";
 import Time from "./components/Time";
+import Rodape from "./components/Rodape";
 
 function App() {
   const times = [
@@ -67,6 +68,7 @@ function App() {
           )}
         />
       ))}
+      <Rodape />
     </div>
   );
 }
---
+++ src/components/Rodape/index.js
@@ -0,0 +1,17 @@
+import "./Rodape.css";
+
+const Rodape = () => {
+  return (
+    <footer className="rodape">
+      <div className="redes-sociais">
+        <img src="/imagens/fb.png" alt="Facebook" />
+        <img src="/imagens/tw.png" alt="Twitter" />
+        <img src="/imagens/ig.png" alt="Instagram" />
+      </div>
+      <img src="/imagens/logo.png" alt="Logo" />
+      <span>Desenvolvido por Alura.</span>
+    </footer>
+  );
+};
+
+export default Rodape;
